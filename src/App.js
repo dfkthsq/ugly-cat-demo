c8e963d686e1e358dec157b02bfe61df3289c1a6
Valerii Skoryk <valeriiskoryk@admins-MacBook-Pro-2.local>
2023-03-08 12:53:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ChakraProvider,
+  Heading,
+  Container,
+  Text,
+  Input,
+  Button,
+  Wrap,
+  Stack, 
+  Image,
+  Link,
+  extendTheme,
+  Spinner,
+  Center,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+// Version 1: Using objects
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: 'gray.600',
+        color: 'white',
+      },
+      // styles for the `a`
+      a: {
+        color: 'teal.500',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+    },
+  },
+})
+
+const App = () => {
+  const [image, updateImage] = useState();
+  const [prompt, updatePrompt] = useState();
+  const [loading, updateLoading] = useState();
+
+  const generate = async (prompt) => {
+    updateLoading(true);
+    const result = await axios.get(`https://phz8nx8jz7.execute-api.eu-west-2.amazonaws.com/Prod/hello?prompt=${prompt}`);
+    updateImage(result.data);
+    updateLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}> 
+      <Container marginTop={"20px"} width={"512px"}>
+        <Heading>Ugly Cat Image Generator 🚀</Heading>
+        <Text marginBottom={"15px"}>
+          This react application is a wrapper for Images API by OPENAI
+          which generates images using the DALL·E model from a description in natural language.
+          But with a little tweak adding an ugly cat to images.
+          More information can be found here{": "}
+          <Link href={"https://platform.openai.com/docs/models/dall-e"} color={"pink"}>
+            OPENAI
+          </Link>
+        </Text>
+
+        <Wrap marginBottom={"15px"}>
+          <Input
+            value={prompt}
+            onChange={(e) => updatePrompt(e.target.value)}
+            width={"370px"}
+            focusBorderColor={"blue.100"}
+          ></Input>
+          <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+            Generate
+          </Button>
+        </Wrap>
+
+        {loading ? (
+          <Stack>
+            <Center>
+              <Spinner
+                thickness='5px'
+                speed='0.75s'
+                emptyColor='gray.200'
+                color='green.300'
+                size='xl'
+                marginTop={"25px"}
+              />
+              </Center>
+          </Stack>
+        ) : image ? (
+          <Image src={image} boxShadow="lg" border={"1px"} />
+        ) : null}
+      </Container>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
